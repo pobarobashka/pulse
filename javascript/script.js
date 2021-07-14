277777809c67f66d9dd1e78fe42529a649a41f80
Pobarobashka <pobarobashka@tut.by>
2021-07-14 12:12:17 +0300

    practice 3.5

--- javascript/script.js
+++ javascript/script.js
@@ -18,3 +18,24 @@ document.querySelector('.prev').addEventListener('click',function () {
 document.querySelector('.next').addEventListener('click',function () {
     slider.goTo("next");
 });
+//jquery
+$(document).ready(function(){
+    //табы
+    $('ul.catalog__tabs').on('click', 'li:not(.catalog__tab_active)', function() {
+        $(this)
+
+            .addClass('catalog__tab_active').siblings().removeClass('catalog__tab_active')
+            .closest('div.container').find('div.catalog__content').removeClass('catalog__content_active').eq($(this).index()).addClass('catalog__content_active ');
+    });
+    function toggleSlide(item){
+        $(item).each(function (i){
+            $(this).on('click',function(e){
+                e.preventDefault();
+                $('.catalog__front').eq(i).toggleClass('catalog__front_active');
+                $('.catalog__back').eq(i).toggleClass('catalog__back_active');
+            })
+        });
+    };
+    toggleSlide('.catalog__link');
+    toggleSlide('.catalog__return')
+});
